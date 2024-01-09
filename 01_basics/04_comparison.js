dd352b3f29514405e9b23fce1eab1efc221c67be
Mukul Padwal <80583870+mukulpadwal@users.noreply.github.com>
2024-01-09 16:52:45 +0000

    [ADD] : comparison-and-equality

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+/*
+NOTE : == is equality and does not change the value on the other hand the comparison operators change the value and then compare
+*/
