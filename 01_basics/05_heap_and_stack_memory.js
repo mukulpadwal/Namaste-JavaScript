91b0ea95688c46314da58baeee0e2ae616ccc782
Mukul Padwal <80583870+mukulpadwal@users.noreply.github.com>
2024-01-10 16:08:25 +0000

    [ADD] : heap_stack_memory_concept

---
+++ 01_basics/05_heap_and_stack_memory.js
@@ -0,0 +1,25 @@
+// The primitive datatypes are stored in the Stack Memory
+// The non-primitive/reference datatypes are stored in the Heap Memory
+
+let myName = 'Mukul'
+let myNameCopy = myName
+myNameCopy = 'MP'
+
+console.log(myName);
+console.log(myNameCopy);
+
+// In a nutshell the copy of the value is passed to the other variable keepin the original one as ot is
+
+let userOne = {
+    name : 'xyz',
+    upiId : 'xyz@ybl'
+}
+
+let userTwo = userOne;
+
+userTwo.name = 'Mukul';
+
+console.log(userOne.name);
+console.log(userTwo.name);
+
+// In a nutshell the location reference of the userOne is given to userTwo
