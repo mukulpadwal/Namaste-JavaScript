2de2be86557cd8f3208d6b51db596389ca31632c
Mukul Padwal <80583870+mukulpadwal@users.noreply.github.com>
2024-01-23 06:42:00 +0000

    REFACTOR : if-else-in-js

--- 04_control_flow/01_if_else.js
+++ 04_control_flow/01_if_else.js
@@ -1,13 +1,16 @@
-// if
+// if, if-else, if-elseif-else
+// Used to run block of code based on certain conditions
+
+
+// 1. if
 
 /*
 SYNTAX : 
-Condition : should be true in order to execute the code inside scope
+    condition : should be true in order to execute the code inside scope
 
-if (condition) {
-    // SCOPE
-
-}
+    if (condition) {
+        // SCOPE
+    }
 */
 
 const isUserLoggedIn = true;
@@ -16,55 +19,88 @@ if(isUserLoggedIn) {
     console.log(`Hello`);
 }
 
-
+// Conditional Operators
 // <, >, >=, <=, ==, !=, ===, !==
 
-// const score = 200;
+const score = 200;
 
-// if(score > 100){
-//     const power = "fly";
-//     console.log(`User Power : ${fly}`);
-// }
-// console.log(`User Power : ${fly}`);
+if(score > 100){
+    const power = "fly";
+    // console.log(`User Power : ${fly}`);
+}
+// console.log(`User Power : ${fly}`); // fly is not defined : Reference Error 
 
 
+// Implicit Scope
 
 const balance = 1000
 
-// Implicit Scope
 // if(balance > 500) console.log('test');
 
-
 // Not Good Code Practice
-// if(balance > 500) console.log('test'), console.log("test2");;
+// if(balance > 500) console.log('test'), console.log("test2");
+
+// 2. if-elseif-else
 
 
-// if(balance < 500) {
-//     console.log(`less than 500`);
-// } else if(balance < 750) {
-//     console.log(`less than 750`);
-// } else if(balance < 900) {
-//     console.log(`less than 900`);
-// } else {
-//     console.log(`default`);
-// }
+/*
+SYNTAX : 
+    condition : should be true in order to execute the code inside scope
+
+    if (condition) {
+        // SCOPE
+    } else if(condition) {
+        //
+    } else if(condition) {
+        //
+    } else {
+        //
+    }
+*/
+
+if(balance < 500) {
+    console.log(`less than 500`);
+} else if(balance < 750) {
+    console.log(`less than 750`);
+} else if(balance < 900) {
+    console.log(`less than 900`);
+} else {
+    console.log(`default`);
+}
+
+// 3. if-else
+
+/*
+SYNTAX : 
+    condition : should be true in order to execute the code inside scope
+
+    if (condition) {
+        // SCOPE
+    } else {
+        // 
+    }
+*/
+
+if(balance < 500) {
+    console.log(`less than 500`);
+} else {
+    console.log(`default`);
+}
 
 
-
-
-// REAL LIFE USE CASE
+// Logical Operators : &&, ||, !
 
 const userLoggedIn = true;
 const debitCard = true;
 const userLoggedInFromGoogle = false;
 const userLoggedInFromEmail = true;
-// Logical Operators
 
+// Both should be true then only the code inside if will execute
 if(userLoggedIn && debitCard) {
     console.log(`Ja Karle Shopping`);
 }
 
-
+// Any one should be true to run the code
 if(userLoggedInFromGoogle || userLoggedInFromEmail){
     console.log(`Kaarle login`);
 }
