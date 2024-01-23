7f77cd9e7209c56be0f4be3831b1180d1708819b
Mukul Padwal <80583870+mukulpadwal@users.noreply.github.com>
2024-01-23 06:48:29 +0000

    REFACTOR : truthy-falsy-js

--- 04_control_flow/03_truthy_falsy.js
+++ 04_control_flow/03_truthy_falsy.js
@@ -1,3 +1,5 @@
+// Sometimes in condition we explicitely don't check for true or false we only pass our variable. javascriot automatically takes treats them as trur or false
+
 const userEmail = "mukul@mukul.com";
 
 if(userEmail){
@@ -6,22 +8,22 @@ if(userEmail){
     console.log(`No email found`);
 }
 
+// NOTES : 
+    // Falsy Values
+    // false, "", 0, -0, BigInt 0n, null, undefined, NaN
 
-// Falsy Values
-// false, "", 0, -0, BigInt 0n, null, undefined, NaN
-
-// Truthy Values
-// {}, [], true, "0",  1, 'false', " ", function(){}, 
+    // Truthy Values
+    // {}, [], true, "0",  1, 'false', " ", function(){}, 
 
 
+// Way to check for empty array : .length property
 const userData = [];
 
 if(userData.length === 0){
     console.log(`Array is Empty`);
 }
 
-
-
+// Way to check for empty object : get the keys an then apply .length property
 const emptyObj = {};
 
 if(Object.keys(emptyObj).length === 0){
@@ -36,32 +38,32 @@ console.log(0 == ''); // true
 
 
 // Nullish Coalescing Operator (??) : null undefined
-
+// It takes the first non null or undefined value if present and assign it to the variable
+// null and undefined acts as fallback mechanism
 let val1;
 
 val1 = 5 ?? 10;
-console.log(val1);
+console.log(val1); // 5
 
 val1 = null ?? 10;
+console.log(val1); // 10
+
+val1 = undefined ?? 15; // 15
+console.log(val1); 
+
+val1 = undefined ?? null; // null
+console.log(val1); 
+
+val1 = null ?? undefined; // undefined
 console.log(val1);
 
-val1 = undefined ?? 15;
-console.log(val1); 
-
-val1 = undefined ?? null;
-console.log(val1); 
-
-val1 = null ?? undefined;
-console.log(val1);
-
-val1 = null ?? undefined ?? 15;
+val1 = null ?? undefined ?? 15; // 15
 console.log(val1); 
 
 
-
-// Terniary Operator
-
+// Terniary Operator : short form for if-else
 // condition ? eval if true : eval if false
+// Do not confuse it with nullish coalescing operator
 
 const iceTeaPrice = 100
 iceTeaPrice <= 80 ? console.log(`Sasti Chai`) : console.log(`Mengi Chai`); 
